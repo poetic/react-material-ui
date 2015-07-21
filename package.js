2109a2e5b874656935118d4f0626b0ede79c41ff
Matthew Hager <matthew@poeticsystems.com>
2015-07-20 22:49:22 -0500

    Update package.js.

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'poetic:react-material-ui',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Everything you need to use material ui inside your Meteor application.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/poetic/react-material-ui.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
