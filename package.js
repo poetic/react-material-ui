94ef1aa28f2e53a0107d0dd969f79d23b2a98ec8
Matthew Hager <matthew@poeticsystems.com>
2015-07-20 23:06:02 -0500

    Minor updates.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:react-material-ui',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Everything you need to use material ui inside your Meteor application.',
   // URL to the Git repository containing the source code for this package.
