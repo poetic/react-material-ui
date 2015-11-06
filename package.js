83b2c7ce98f7a89e4b9f8438c75b81b034f236a9
codeNpaper <kenoduah@poeticsystems.com>
2015-11-05 22:42:54 -0600

    update version and fix example app themeing bug

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:react-material-ui',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'Everything you need to use material ui inside your Meteor application.',
   // URL to the Git repository containing the source code for this package.
